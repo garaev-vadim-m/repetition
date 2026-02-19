25ac04467d6d0f8d6cafe5bd8d4ae87b6aa2d467
v.garaev <v.garaev@csat.ru>
2026-02-19 16:21:10 +0300

    format ai

--- javascript/basics/lesson-6/object.js
+++ javascript/basics/lesson-6/object.js
@@ -1,98 +1,176 @@
-let obj1 = new Object(); // синтаксис "конструктор объекта"
+// ============= СОЗДАНИЕ ОБЪЕКТОВ =============
 
-let obj2 = {}; // синтаксис "литерал объекта"
+// Создание объектов разными способами
+const obj1 = new Object(); // синтаксис "конструктор объекта" - устаревший способ
+const obj2 = {}; // синтаксис "литерал объекта" - современный и предпочтительный способ
 
-console.log(obj1);
-console.log(obj2);
+console.log('Объекты после создания:');
+console.log('obj1 (конструктор):', obj1);
+console.log('obj2 (литерал):', obj2);
 
-obj1.name = 'name1'; //в конструкторе объекта можно создать ключ. Но это не правильно
-obj2.name = 'name'; //создаем ключ и записываем значение ключа. В литеральном обьекте "правильно".
+// ============= ДОБАВЛЕНИЕ СВОЙСТВ =============
 
+// Простое добавление свойств (не рекомендуется для конструктора)
+obj1.name = 'name1'; // так можно, но лучше использовать литералы
+obj2.name = 'name'; // в литеральном объекте так делать "правильно"
+
+// Правильный способ добавления свойств через defineProperty
 Object.defineProperty(obj2, 'female', {
   value: 'very hot',
-  writable: true,
-  enumerable: true,
-  configurable: true,
-}); // корректное создание ключа для конструктора объектов
+  writable: true, // можно изменять значение
+  enumerable: true, // будет видно при перечислении
+  configurable: true, // можно удалять и изменять атрибуты
+});
 
-console.log(obj1);
-console.log(obj2);
-console.log(obj1['name']);
-console.log(obj2.female);
+console.log('\nПосле добавления простых свойств:');
+console.log('obj1:', obj1);
+console.log('obj2:', obj2);
+console.log('obj1["name"]:', obj1['name']);
+console.log('obj2.female:', obj2.female);
 
-Object.defineProperty(obj2, 'neskolso slov', {
+// Свойства с пробелами в имени
+Object.defineProperty(obj2, 'neskolko slov', {
   value: 'esli tochnee - dva slova',
   writable: true,
   enumerable: true,
   configurable: true,
-}); // Можем создать два слова, но только в ковычках
+});
 
-console.log(obj2);
-console.log(obj2['neskolso slov']); //и обратиться можем только так
+console.log('\nСвойство с пробелами в имени:');
+console.log('obj2:', obj2);
+console.log('obj2["neskolko slov"]:', obj2['neskolko slov']); // доступ только через квадратные скобки
 
+// ============= РАСШИРЕНИЕ ОБЪЕКТОВ =============
+
+// Динамическое создание ключа
 const KEY_NAME = 'aaaaa';
 
-obj1 = {
-  ...obj1, //Можем перезаписать объект и добавить прерыдушие свойта которые записали
-  ...obj2, //Можем расширить данные объекта
-  [KEY_NAME]: 'bbbb', //можем и так ключ создать\
+// Перезапись объекта с расширением
+const obj1Updated = {
+  ...obj1, // копируем все свойства из obj1
+  ...obj2, // добавляем свойства из obj2
+  [KEY_NAME]: 'bbbb', // создаем ключ из переменной
 };
 
-console.log(obj1);
+console.log('\nОбновленный obj1:');
+console.log(obj1Updated);
 
-function userData(name, age) {
+// ============= ФАБРИКА ОБЪЕКТОВ =============
+
+/**
+ * Функция-фабрика для создания объектов пользователей
+ * @param {string} name - имя пользователя
+ * @param {number} age - возраст
+ * @returns {Object} объект с данными пользователя
+ */
+function createUser(name, age) {
   return {
-    name,
-    age,
+    name, // сокращенная запись (вместо name: name)
+    age,  // сокращенная запись (вместо age: age)
   };
 }
 
-console.log(userData('AAAAA', 12));
+console.log('\nФабрика объектов:');
+console.log(createUser('AAAAA', 12));
 
-console.log('AAAAAAA' in obj1);
+// ============= ПРОВЕРКА СУЩЕСТВОВАНИЯ СВОЙСТВ =============
 
-obj2 = {
+console.log('\nПроверка существования свойства "AAAAAAA" в obj1:');
+console.log('AAAAAAA' in obj1Updated); // оператор in проверяет наличие свойства
+
+// ============= МЕТОДЫ ОБЪЕКТА =============
+
+// Обновляем obj2 с добавлением методов
+const obj2WithMethods = {
   ...obj2,
-  func: function () {
-    console.log(this.name);
+  
+  // Обычная функция как метод
+  showName: function() {
+    console.log('Имя из метода showName:', this.name);
   },
-};
-
-obj2.func();
-
-obj2 = {
-  ...obj2,
-  func: function () {
-    console.log(this.name);
+  
+  // Сокращенный синтаксис методов (ES6)
+  showFemale() {
+    console.log('female из метода showFemale:', this.female);
   },
-
-  func2() {
-    console.log(this.female);
+  
+  // Метод с проверкой наличия свойств
+  checkProperties() {
+    console.log('"female" in this:', 'female' in this);
+    console.log('"notfemale" in this:', 'notfemale' in this);
   },
-
-  func3() {
-    console.log('female' in this);
-    console.log('notfemale' in this);
-  },
-
+  
+  // Метод со стрелочной функцией внутри
   sayHello() {
-    let arrow = () => console.log(this.name, 'стрелочная функция');
-    arrow(); //нет своего this. В таком случае будет ссылка на функцию sayHello
+    // Стрелочная функция не имеет своего this, 
+    // поэтому берет this из внешней функции sayHello
+    const arrow = () => {
+      console.log('Привет от стрелочной функции:', this.name);
+    };
+    arrow();
   },
-
-  //   arrowFunc: () => console.log(this.name), //Error нет своего this
+  
+  // Стрелочная функция как метод (НЕ РАБОТАЕТ КАК ОЖИДАЕТСЯ!)
+  // arrowFunc: () => console.log(this.name), // this будет глобальным объектом
 };
 
-obj2.func2();
-obj2.func3();
-obj2.sayHello();
-// obj2.arrowFunc();
+// Сохраняем обновленный объект обратно в obj2
+// obj2 = obj2WithMethods; // закомментировано, чтобы не потерять ссылки
 
-let a1 = obj2; // ссылка на бъект
-let a2 = { ...obj2 }; // копирование объекта
+// Для демонстрации создадим новый объект
+const demoObj = obj2WithMethods;
 
-a1.privert = 'privet';
+console.log('\nВызов методов объекта:');
+demoObj.showName();
+demoObj.showFemale();
+demoObj.checkProperties();
+demoObj.sayHello();
 
-a2.poka = 'poka';
+// ============= КОПИРОВАНИЕ ОБЪЕКТОВ =============
 
-console.log('\n a1', a1, obj2, '\n a2', a2, obj2);
+// Создаем тестовый объект
+const originalObject = {
+  name: 'Original',
+  age: 25
+};
+
+// Ссылка на объект (не копия!)
+const objectReference = originalObject;
+
+// Поверхностное копирование объекта
+const objectCopy = { ...originalObject };
+
+// Добавляем свойства для демонстрации различий
+objectReference.newProperty = 'добавлено через ссылку';
+objectCopy.anotherProperty = 'добавлено в копию';
+
+console.log('\n=== ДЕМОНСТРАЦИЯ ССЫЛОК И КОПИРОВАНИЯ ===');
+console.log('Оригинал:', originalObject);
+console.log('Ссылка на объект:', objectReference);
+console.log('Копия объекта:', objectCopy);
+
+console.log('\nОбратите внимание:');
+console.log('- Свойство, добавленное через ссылку, появилось в оригинале');
+console.log('- Свойство, добавленное в копию, НЕ появилось в оригинале');
+
+// ============= ДОПОЛНИТЕЛЬНЫЕ ПРИМЕРЫ =============
+
+// Дескрипторы свойств
+console.log('\n=== ДЕСКРИПТОРЫ СВОЙСТВ ===');
+const propertyDescriptor = Object.getOwnPropertyDescriptor(demoObj, 'female');
+console.log('Дескриптор свойства "female":', propertyDescriptor);
+
+// Получение всех ключей объекта
+console.log('\nКлючи объекта demoObj:', Object.keys(demoObj));
+console.log('Значения объекта demoObj:', Object.values(demoObj));
+console.log('Пары ключ-значение:', Object.entries(demoObj));
+
+// Заморозка объекта (делает неизменяемым)
+const frozenObject = Object.freeze({
+  name: 'Frozen',
+  value: 42
+});
+
+// frozenObject.name = 'New Name'; // Ошибка в strict mode, игнорируется в обычном режиме
+console.log('\nЗамороженный объект:', frozenObject);
+console.log('Объект заморожен?', Object.isFrozen(frozenObject));
